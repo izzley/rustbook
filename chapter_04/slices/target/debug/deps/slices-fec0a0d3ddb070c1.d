8152d30c61f83d1138cc0f9b5342f2ac687a8c44
izz <izzl@protonmail.com>
2022-02-28 08:25:55 +1100

    FEAT: slices

---
+++ chapter_04/slices/target/debug/deps/slices-fec0a0d3ddb070c1.d
@@ -0,0 +1,5 @@
+/home/izzlee/repos/rustbook/chapter_04/slices/target/debug/deps/slices-fec0a0d3ddb070c1: src/main.rs
+
+/home/izzlee/repos/rustbook/chapter_04/slices/target/debug/deps/slices-fec0a0d3ddb070c1.d: src/main.rs
+
+src/main.rs:
