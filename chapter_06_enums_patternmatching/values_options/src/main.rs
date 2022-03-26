dbc3a3a5f3a06dac4e7c7632378401531655d447
izz <izzl@protonmail.com>
2022-03-26 19:22:29 +1100

    FEAT: chapter 6 enums and target/ .gitignore

--- chapter_06_enums_patternmatching/values_options/src/main.rs
+++ chapter_06_enums_patternmatching/values_options/src/main.rs
@@ -1,3 +1,102 @@
+// fn main() {
+//     let localhost: IpAddrKind::V4(127, 0, 0, 1);
+
+// }
+
+// enum IpAddrKind {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// enum Message {
+//     Quit,
+//     Move { x:i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+// impl Message {
+//     fn some_function() {
+//         println!("Lets get rust!")
+//     }
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+
+// ###################
+
+// fn main() {
+//     enum Option<T> {
+//         Some(T),
+//         None,
+//     }
+// }
+
+
+// ###############
+
+// fn main() {
+//     value_in_cents(Coin::Quarter(UsState::Alaska));
+// }
+
+// #[derive(Debug)] // so we can inspect the state when compiling
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     Arizona,
+//     Arkansas,
+//     California,
+//     //...
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => {
+//             println!("State quarter from {:?}!", state);
+//             25,
+//         }
+//     }
+// }
+
+// ########################
+
+// fn main() {
+//     let five: Option<i32> = Some(5);
+//     let _six: Option<i32> = plus_one(five);
+//     let _none: Option<i32> = plus_one(None);
+
+// }
+
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i: i32) => Some(i + 1),
+//     }
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let some_value: Option<i32> = Some(3);
+    
+    match some_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+    
+    if let Some(3) = some_value {
+        println!("three");
+    }
 }
